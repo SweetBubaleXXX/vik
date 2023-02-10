e9d1b3953b6f3305644db7ae725f558b2580a442
debill <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-02-10 18:51:18 +0300

    Release 1.0

--- vik/vik.py
+++ vik/vik.py
@@ -3,12 +3,12 @@
 import os
 import subprocess
 import sys
-from importlib.resources import files
+from importlib import resources
 from tempfile import TemporaryDirectory
 
 
 def _create_vimrc_file(directory: str) -> str:
-    vimrc = files("vik").joinpath(".vimrc").read_text()
+    vimrc = resources.files("vik").joinpath(".vimrc").read_text()
     vimrc_path = os.path.join(directory, ".vimrc")
     with open(vimrc_path, "w") as f:
         f.write(vimrc)
@@ -17,6 +17,9 @@ def _create_vimrc_file(directory: str) -> str:
 
 def main():
     with TemporaryDirectory() as tmp_dir:
+        if len(sys.argv) < 2:
+            sys.stderr.write("File not specified\n")
+            sys.exit(2)
         vimrc_path = _create_vimrc_file(tmp_dir)
         subprocess.run(["vim", "-u", vimrc_path, sys.argv[1]])
 
